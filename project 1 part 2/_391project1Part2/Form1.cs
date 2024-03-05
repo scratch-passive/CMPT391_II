66e33f4d8af97cc5acd1bc53cdfb08783f0aae8f
Jackson Steed <118392821+jacksonsteed@users.noreply.github.com>
2024-03-04 17:12:03 -0700

    initializing form1

---
+++ project 1 part 2/_391project1Part2/Form1.cs
@@ -0,0 +1,20 @@
+namespace _391project1Part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
